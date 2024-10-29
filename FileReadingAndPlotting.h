192b74086f1d68b3f8426dd1726f03c3016e37f5
CalebG6 <calebgottlieb@ufl.edu>
2024-10-29 15:50:53 -0400

    matplot++

---
+++ FileReadingAndPlotting.h
@@ -0,0 +1,15 @@
+//
+// Created by caleb on 10/29/2024.
+//
+
+#ifndef DSA_PROJECT_3_FILEREADINGANDPLOTTING_H
+#define DSA_PROJECT_3_FILEREADINGANDPLOTTING_H
+#include<iostream>
+#include <matplot/matplot.h>
+
+
+
+
+
+
+#endif //DSA_PROJECT_3_FILEREADINGANDPLOTTING_H
