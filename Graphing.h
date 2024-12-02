d0c8efb5f28fcf26debef8630d7f99449f08f8cb
CalebG6 <calebgottlieb@ufl.edu>
2024-12-02 18:19:49 -0500

    Added line labels

--- Graphing.h
+++ Graphing.h
@@ -7,8 +7,8 @@
 #pragma once
 
 struct Graph{
-    void graphTimes(std::vector<School*> schools){
-        std::pair<std::vector<float>, std::vector<float>> times = dataCall(schools);
+    void graphTimes(std::vector<School*> schools,std::string comparer, bool ascending){
+        std::pair<std::vector<float>, std::vector<float>> times = dataCall(schools,comparer,ascending);
         std::vector<float> merge = times.first;
         std::vector<float> quick = times.second;
         int size = schools.size();
@@ -16,32 +16,36 @@ struct Graph{
         int num_points = size/increment + 1;
         std::vector<double> x_axis = matplot::linspace(0,size,num_points);
         matplot::hold(true);
+        matplot::cla();
         auto p1 = matplot::plot(x_axis,merge);
         p1->display_name("Merge Sort");
         auto p2 = matplot::plot(x_axis,quick);
         p2->display_name("Quick Sort");
 
         matplot::legend();
+        matplot::xlabel("Number of Schools");
+        matplot::ylabel("Execution Time(s)");
 
         matplot::show();
     }
 
-    std::pair<std::vector<float>,std::vector<float>> dataCall(std::vector<School*> schools){ //This is an un sorted version so we may have to change how we sort
+    std::pair<std::vector<float>,std::vector<float>> dataCall(std::vector<School*> schools, std::string comparer, bool ascending){ //This is an un sorted version so we may have to change how we sort
         int size = schools.size();
         int increment = std::max(5,size/10);
         int i = increment;
         std::vector<float> merge_time;
         std::vector<float> quick_time;
+        auto compare_function = getComparisonFunction(comparer,ascending);
         while(i<size){
             std::vector<School*> temp_m(schools.begin(),schools.begin()+i);
             std::vector<School*> temp_q(schools.begin(),schools.begin()+i);
             auto start = std::chrono::high_resolution_clock::now();
-            //Call merge sort with temp_m
+            mergeSort(temp_m,0,temp_m.size()-1,compare_function);
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<float> duration = end - start;
             merge_time.push_back(duration.count());
             start = std::chrono::high_resolution_clock::now();
-            //Call quick sort with temp_q
+            quickSort(temp_q,0,temp_q.size()-1,compare_function);
             end = std::chrono::high_resolution_clock::now();
             duration = end - start;
             quick_time.push_back(duration.count());
